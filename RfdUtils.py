3d46c75e45b435417c9b57c54a491f4d076506f6
Oceanbreak <oceanbreak@gmail.com>
2019-02-15 11:42:04 +0300

    RfdUtil ver. 0.1.1 --update

--- RfdUtils.py
+++ RfdUtils.py
@@ -5,8 +5,6 @@ version 0.1.1
 import sys
 import Utils
 from skimage import io
-from skimage import img_as_float as imf
-from skimage.color import rgb2gray
 from skimage.filters import gaussian
 from matplotlib import pyplot as plt
 import numpy as np
@@ -14,7 +12,7 @@ from math import atan2, sqrt, pi
 
 class RFD:
     def __init__(self, input_img, show_steps = False):
-        self._input_img = Utils.readImPatch(input_img)
+        self._input_img = input_img
         self._im_height, self._im_width = self._input_img.shape
         self._show_steps = show_steps
 
@@ -23,49 +21,12 @@ class RFD:
         for i in range(self._orient_quant + 1):
             self._gradMap.append(np.zeros((self._im_height, self._im_width)))
 
-class RfdUtils:
-    # Class that reads image and seprates it into set of gradients in 8 directions
-    # Input: RGB or Grayscale image
-    def __init__(self, input_image, show_steps = False):
-        if len(input_image) > 20:
-            self._im_name = input_image[:3] + "..." + input_image[-20:]
-        else:
-            self._im_name = input_image
-        self._input_image = io.imread(input_image)
-        self._show_steps = show_steps    # Enables to plot each step of program
-
-        # Convert to grayscale and then to float array
-        self._in_gray_img = rgb2gray(self._input_image)
-        self._in_float_img = imf(self._in_gray_img)
-
-        # Calculate height and width
-        self._im_height = self._input_image.shape[0]
-        self._im_width = self._input_image.shape[1]
-
-        self._orient_quant = 8  # Quantization of gradient directions
-        self._gradMap = []      # Initialize output gradients array
-        for i in range(self._orient_quant+1):
-            self._gradMap.append(np.zeros((self._im_height, self._im_width)))
-
-        self.show(self._in_float_img, 'Grayscaled input image "%s"' % self._im_name)
-
     def show(self, img, name):
         if self._show_steps:
             io.imshow(img)
             plt.title(name)
             plt.show()
 
-    def showGradients(self):
-        f = plt.figure()
-        for array, num in zip(self._gradMap, range(1, len(self._gradMap))):
-            ax = f.add_subplot(2, self._orient_quant/2, num)
-            ax.margins(0)
-            io.imshow(array)
-            ax.set_title(str(num))
-        f.tight_layout()
-        plt.show()
-
-    # This function is almost identical copy of exactly the same function in RFD source code
     def getHoG(self):
         print('Calculating gradient bins ...')
         nDir = self._orient_quant
@@ -73,30 +34,30 @@ class RfdUtils:
         height = self._im_height
         width = self._im_width
         sigma0 = 1.6
-        smooth_patch = gaussian(self._in_gray_img, sigma0)
+        smooth_patch = gaussian(self._input_img, sigma0)
 
         hog = np.zeros(nDir * width * height)
 
-        for y in range(GRAD_RADIUS, height-1-GRAD_RADIUS):
-            for x in range(GRAD_RADIUS, width-1-GRAD_RADIUS):
-                gray2 = smooth_patch[y, x+GRAD_RADIUS]
-                gray1 = smooth_patch[y, x-GRAD_RADIUS]
-                dx = gray2 - gray1
-                gray2 = smooth_patch[y+GRAD_RADIUS, x]
-                gray1 = smooth_patch[y-GRAD_RADIUS, x]
-                dy = gray2 - gray1
+        for y in range(GRAD_RADIUS, height - 1 - GRAD_RADIUS):
+            for x in range(GRAD_RADIUS, width - 1 - GRAD_RADIUS):
+                grad2 = smooth_patch[y, x + GRAD_RADIUS]
+                grad1 = smooth_patch[y, x - GRAD_RADIUS]
+                dx = grad2 - grad1
+                grad2 = smooth_patch[y + GRAD_RADIUS, x]
+                grad1 = smooth_patch[y - GRAD_RADIUS, x]
+                dy = grad2 - grad1
 
                 # Calculate direction and magnitude of gradient
-                dir = atan2(dy, dx)
-                mag = sqrt(dx * dx + dy * dy)
+                dir = atan2(dy, dx)             # Direction
+                mag = sqrt(dx * dx + dy * dy)   # Magnitude
 
                 # Assign direction to pixel
                 idxDir = (dir + pi) * nDir / (2.0 * pi)
                 if int(idxDir) == nDir: idxDir -= nDir
 
                 # Soft assignment of direction with weights
-                dirIdx = [0,0]
-                dirWeight = [0,0]
+                dirIdx = [0, 0]
+                dirWeight = [0, 0]
                 dirIdx[0] = int(idxDir)
                 dirIdx[1] = (dirIdx[0] + 1) % nDir
                 dirWeight[0] = 1.0 - (idxDir - dirIdx[0])
@@ -104,7 +65,7 @@ class RfdUtils:
 
                 # Calculate current position in one-dimensional array
                 pos_idx = y * width + x
-                hog[dirIdx[0]*width*height + pos_idx] = dirWeight[0] * mag
+                hog[dirIdx[0] * width * height + pos_idx] = dirWeight[0] * mag
                 hog[dirIdx[1] * width * height + pos_idx] = dirWeight[1] * mag
 
         for y in range(height):
@@ -113,8 +74,10 @@ class RfdUtils:
                 for i in range(nDir):
                     self._gradMap[i][y, x] = hog[i * width * height + y * width + x]
                     Total += hog[i * width * height + y * width + x]
-                self._gradMap[nDir][y,x] = Total
+                self._gradMap[nDir][y, x] = Total
 
+        for i, array in enumerate(self._gradMap):
+            self.show(array, str(i))
 
 
 
@@ -122,7 +85,7 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         sys.stderr.write('Usage: python %s <inputfile> \n' % sys.argv[0])
         raise SystemExit(1)
-    img_to_process = RfdUtils(sys.argv[1], True)
+    input_img = Utils.readImPatch(sys.argv[1])
+    input_img = Utils.cropImg(input_img, (0,0), (64,64))
+    img_to_process = RFD(input_img, True)
     img_to_process.getHoG()
-    img_to_process.showGradients()
-
