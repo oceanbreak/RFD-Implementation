572856098da2377f24413f21618b60387ea484ad
Oceanbreak <oceanbreak@gmail.com>
2019-02-16 15:24:52 +0300

    ManageDataset function createIntegrals working for Patch dataset

--- ManageDataset.py
+++ ManageDataset.py
@@ -1,31 +1,42 @@
-import Utils
+"""
+Preparing patch dataset
+"""
 
+import Utils
 from skimage.transform import integral as intg
 from tkinter import Tk
 from skimage import io
-from skimage.color import rgb2gray
-from skimage import img_as_float as imf
 from tkinter import filedialog
 from matplotlib import pyplot as plt
 import numpy as np
 
-root = Tk()
-input_file = filedialog.askopenfilename()
-root.destroy()
-img = Utils.readImPatch(input_file)
-#img = Utils.cropImg(img, (0,0), (20,3))
-io.imshow(img)
-plt.title('Input')
-plt.show()
+PATCHES_ARRAY_SIZE = (16, 16)
+PATCH_SIZE = (64, 64)
 
-height, width  = img.shape
-print(img.shape)
+def createIntegralImages():
+    root = Tk()
+    input_file_list = filedialog.askopenfilenames()
+    root.destroy()
+    for  input_file in input_file_list:
+        img = Utils.readImPatch(input_file)
+        print('Processing ' + input_file)
 
-img_integral = intg.integral_image(img)
-plt.imshow(img_integral)
-plt.colorbar()
-plt.show()
+        # Initialize array for integral images
+        img_int = np.zeros(img.shape)
 
-output_file = '.'.join(input_file.split('.')[:-1]) + '_integral.tif'
-io.imsave(output_file, img_integral)
+        for j in range(PATCHES_ARRAY_SIZE[0]):
+            for i in range(PATCHES_ARRAY_SIZE[1]):
+                im_patch = Utils.cropImg(img, (i*PATCH_SIZE[0], j*PATCH_SIZE[1]),
+                                         (i*PATCH_SIZE[0] + PATCH_SIZE[0], j*PATCH_SIZE[1] + PATCH_SIZE[1]))
 
+                img_integral = intg.integral_image(im_patch)
+
+                img_int[j*PATCH_SIZE[1] : j*PATCH_SIZE[1] + PATCH_SIZE[1],
+                        i*PATCH_SIZE[0] : i*PATCH_SIZE[0] + PATCH_SIZE[0]] = img_integral
+
+
+        output_file = '.'.join(input_file.split('.')[:-1]) + '_integral.tif'
+        io.imsave(output_file, img_int)
+
+if __name__ == '__main__':
+    createIntegralImages()
