4a1046095001418d2757fd433dd9cf5615fd448f
oceanbreak <oceanbreak@gmail.com>
2019-04-15 10:04:08 +0300

    Counting vector-like weighted sum for sliced peace of dataset

--- LearnDescriptor_Test.py
+++ LearnDescriptor_Test.py
@@ -1,25 +1,25 @@
 
 import Utils
 import numpy as np
+import os
 from sys import stdout
 from matplotlib import pyplot as plt
 
-DATASET = '/media/sf_Share/Patch_Dataset_Integral.npy'
-DATASET_INFO = '/media/sf_Share/Patch_Dataset_Info_FPG.npy'
+# DATASET = '/media/sf_Share/Patch_Dataset_Integral.npy'
+# DATASET_INFO = '/media/sf_Share/Patch_Dataset_Info_FPG.npy'
 TEMP_FILE = 'temp.dat'
 
-# DATASET = '/home/oceanbreak//Documents/IPPI/Datasets/Patch_Dataset_Integral.npy'
-# DATASET_INFO = '/home/oceanbreak//Documents/IPPI/Datasets/Patch_Dataset_Info_FPG.npy'
-dataset_cut = 25000
+DATASET = '/home/oceanbreak//Documents/IPPI/Datasets/Patch_Dataset_Integral.npy'
+DATASET_INFO = '/home/oceanbreak//Documents/IPPI/Datasets/Patch_Dataset_Info_FPG.npy'
+dataset_cut = 10000
 
 dataset_raw = np.load(DATASET, mmap_mode='r')
-dataset = np.zeros((dataset_cut, 64, 64, 9))
-
 dataset_info = np.load(DATASET_INFO)
-dataset[:,:,:] = dataset_raw[:dataset_cut, :, :, 1:10]
+
+
 im_num = 12
 im_chan = 6
-print(dataset.shape)
+
 # Utils.showImage(dataset[im_num,:,:,im_chan])
 
 rectangle_param = ((2,4), (32,28))  # Choose one rectangle, top left pixel and bottom right pixel
@@ -27,68 +27,64 @@ rect_param_set = ( ((2,4), (32,26)),
                    ((4,6), (10, 42)),
                    ((0,0), (16,28)))
 
-shape_arr_val = (len(rect_param_set), dataset.shape[0], dataset.shape[-1])
-# value = np.memmap(TEMP_FILE, dtype=np.float32, mode='w+', shape=shape_arr_val)
-value = np.zeros(shape_arr_val)
+
 
 # initialize array of responses for all channels and one rectangle
 # response_set = np.zeros((dataset.shape[0], 8))
 
-def manualCalcRect(rectangle_param):
+def manualCalcRect(dataset, rectangle_param):
     print('Rectangle values:')
     pixel_values = []
+    zd_values = []
     for i in range(2):
         for j in range(2):
             x = rectangle_param[i][0]
             y = rectangle_param[j][1]
             v = dataset[im_num, y, x, im_chan]
+            zd = dataset[im_num, y, x, -1]
             print('x = %i, y = %i : value = %f' % (x,y,v))
             pixel_values.append(v)
+            zd_values.append(zd)
     rect_sum = pixel_values[3] + pixel_values[0] - pixel_values[1] - pixel_values[2]
+    w_r_sum = rect_sum / (zd_values[3] + zd_values[0] - zd_values[1] - zd_values[2])
     print('Rectangle sum: %f' % rect_sum)
+    print('Weighted Sum: %f ' % w_r_sum)
+    print()
 
 
-def calcArraySum(rectangle_param, index):
-    # pixel_values = np.zeros((dataset.shape[0], 8, 4))
-    # pixel_values = np.memmap('temp.dat', dtype=np.float32, mode='w+', shape=shape_arr)
-    # value = np.memmap('temp.dat', dtype=np.float32, mode='w+', shape=shape_arr)
-    # print(pixel_values.shape)
+def calcArraySum(dataset, rectangle_param, index):
     print('Assigning values')
     rp = []
     k = 0
     for i in range(2):
         for j in range(2):
-            print('Iteraton %i' % k)
-
             # pixel_coords.append((rectangle_param[i][0], rectangle_param[j][1]))
             x = rectangle_param[i][0]
             y = rectangle_param[j][1]
-
-            if k==0 or k==3: sign = 1
-            else: sign = -1
-
             rp.append((y,x))
 
-            # value = value + sign * dataset[:, y, x, 1:9]
+    zd = dataset[:, rp[0][0], rp[0][1], -1] + \
+            dataset[:, rp[3][0], rp[3][1], -1] - \
+            dataset[:, rp[1][0], rp[1][1], -1] - \
+            dataset[:, rp[2][0], rp[2][1], -1]
 
-            k += 1
-    # global value
-    value[index,:,:] = dataset[:, rp[0][0], rp[0][1], 0:9] + \
-            dataset[:, rp[3][0], rp[3][1], 0:9] - \
-            dataset[:, rp[1][0], rp[1][1], 0:9] - \
-            dataset[:, rp[2][0], rp[2][1], 0:9]
+
+    value[index,:,:] = (dataset[:, rp[0][0], rp[0][1], :-1] + \
+            dataset[:, rp[3][0], rp[3][1], :-1] - \
+            dataset[:, rp[1][0], rp[1][1], :-1] - \
+            dataset[:, rp[2][0], rp[2][1], :-1] ) / zd[None, :, None]
     print('End of calculation')
     print('Calculated throug array size:')
-    print(value.shape)
-    print('Rectangle sum %f' % value[index, im_num, im_chan])
+    print('Rectangle sum %f ' % value[index, im_num, im_chan])
+
+    # print('Weighted sum %f ' % (value[index, im_num, im_chan] / value[index, im_num, -1]) )
+    print()
 
 
-# def testNpAr():
-#     arr1 = np.array([0.001*i*13%23 for i in range(3000000)])
-#     arr2 = np.array([0.001*i*91%37 for i in range(3000000)])
-#     print(arr1+arr2*arr1)
-# testNpAr()
+dataset = dataset_raw[:dataset_cut, :, :, 1:10]
+shape_arr_val = (len(rect_param_set), dataset.shape[0], dataset.shape[-1]-1)
+value = np.zeros(shape_arr_val)
 
 for i, r in enumerate(rect_param_set):
-    manualCalcRect(r)
-    calcArraySum(r, i)
+    manualCalcRect(dataset, r)
+    calcArraySum(dataset, r, i)
