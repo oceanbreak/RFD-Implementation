3efa258719edb7892333ca5227040463b155b93d
oceanbreak <oceanbreak@gmail.com>
2019-06-13 09:51:33 +0300

    Learning OK but slow

--- LearnDescriptor.py
+++ LearnDescriptor.py
@@ -5,55 +5,159 @@ for rectangular areas of RFD
 """
 
 import numpy as np
-from RFD import RFD
+import psutil
+from RectangleCalculation import candidateRects
 from sys import stdout
-import Utils
-from matplotlib import pyplot as plt
+import struct
 
-DATASET = '/media/sf_Share/Patch_Dataset_Integral.npy'
-DATASET_INFO = '/media/sf_Share/Patch_Dataset_Info_FPG.npy'
+# Initializing dataset array, fpg, and rectangle list
+DATASET = '/home/oceanbreak//Documents/IPPI/Datasets/Patch_Dataset_Integral.npy'
+DATASET_INFO = '/home/oceanbreak//Documents/IPPI/Datasets/Patch_Dataset_Info_FPG.npy'
 
-dataset = np.load(DATASET, mmap_mode='r')
+dataset_raw = np.load(DATASET, mmap_mode='r')
 dataset_info = np.load(DATASET_INFO)
-dataset = dataset[:1000]
-print(dataset.shape)
+rectangle_set = candidateRects(*dataset_raw.shape[1:3])
+threshold_set = np.empty([len(rectangle_set), 8], dtype='float32')
 
-response_set = np.zeros((dataset.shape[0], 8))
-cur_threshold = 0.1
-rectangle_param = (3, (0,0), (32,32))
+print('Loaded rectangle set of length %s' % len(rectangle_set))
+print('Loaded dataset of shape %s' % str(dataset_raw.shape))
+
+rect_set_slice = 150
+dataset_slice = 5000
+
+# Temrporary arrays for storing calculated responses and input dataset sliced into parts
+temp_dataset_array = np.zeros([dataset_slice, 64, 64, 9], dtype='float32')
+temp_response_array = np.zeros([rect_set_slice, dataset_raw.shape[0], 8], dtype='float32')
+
+print(psutil.virtual_memory().free)
+
+def binarResponses(input_array, threshold):
+    """
+    Binarizing responses, where input array is array of responses for some area
+    """
+    return input_array > threshold
+
+def calcOnesZeros(input_array, fpg):
+    """
+    Function calculates 1s and 0s responses in one group
+    input array - array of Trues and Falses that are responses of RFD
+    fpg - array of First Patches in Group index
+    """
+    ones_zeros = []
+    for i in range(len(fpg)-1):
+        ones = 0
+        zeros = 0
+        for j in range(fpg[i], fpg[i+1]):
+            if input_array[j]: ones += 1
+            else: zeros += 1
+        ones_zeros.append( (ones, zeros) )
+    return tuple(ones_zeros)
+
+def calcTP(ones_zeros):
+    """
+    True Positive rate calculation for current response set
+    """
+    tp_up = 0
+    tp_down = 0
+    for i in range(len(ones_zeros)):
+        x = ones_zeros[i][0]
+        y = ones_zeros[i][1]
+        tp_up += x*(x-1) + y*(y-1)
+        tp_down += (x+y)*(x+y-1)
+    return tp_up / tp_down
+
+def calcTN(ones_zeros):
+    """
+    True negative rate calculation for current response set
+    """
+    xsum = 0
+    ysum = 0
+    for x, y in ones_zeros:
+        xsum += x
+        ysum += y
+    tn_up = 0
+    tn_down = 0
+    for  i in range(len(ones_zeros)):
+        x = ones_zeros[i][0]
+        y = ones_zeros[i][1]
+        tn_up += x*(ysum-y) + y*(xsum-x)
+        tn_down += (xsum+ysum-x-y)*(x+y)
+    return tn_up / tn_down
+
+def writeThresholdValue(filename, ytop, xtop, ybot, xbot, channel, threshold):
+    with open(filename, 'a+b') as binary_file:
+        entry = struct.pack('<HHHHHf', ytop, xtop, ybot, xbot, channel, threshold)
+        binary_file.write(entry)
+        binary_file.flush()
+
+def calcRectResponse(dataset, dsbegin, dsend, ytop, xtop, ybot, xbot, rect_index, rect_response):
+    """
+    Takes a dataset, calculates response for one specified rectangle
+    and stores value in rect_response array.
+    """
+    stdout.write('\rCalculating rectangle %i' % rect_index)
+
+    zd = dataset[:, ytop, xtop, -1] + \
+            dataset[:, ybot, xbot, -1] - \
+            dataset[:, ybot, xtop, -1] - \
+            dataset[:, ytop, xbot, -1]
+
+    rect_response[rect_index, dsbegin:dsend, :] = (dataset[:(dsend-dsbegin), ytop, xtop, :-1] +
+            dataset[:(dsend-dsbegin), ybot, xbot, :-1] -
+            dataset[:(dsend-dsbegin), ybot, xtop, :-1] -
+            dataset[:(dsend-dsbegin), ytop, xbot, :-1]) / zd[None, :(dsend-dsbegin), None]
 
 
-# def calculateRfd(i):
-#     img_descriptor = RFD(dataset[i], learn=True)
-#     response_set[i]=(img_descriptor.rfResponseRect(*rectangle_param, cur_threshold, integral=True))
+if __name__ == '__main__':
+    """
+    Main loop: iterating through rectangles, slicing dataset
+    and calculating responses, then TP and TN rate for each rectangle,
+    then storing into threshold_set array.
+    """
 
-def calculateRfd(i):
-    zd = Utils.calcRectSum(dataset[i, :, :, -1], rectangle_param[1], rectangle_param[2], integral=True)
-    for channel_num in range(8):
-        cur_channel = Utils.calcRectSum(dataset[i, :, :, channel_num+1], rectangle_param[1], rectangle_param[2], integral=True)
-        response_set[i, channel_num] = cur_channel/ zd
+    for rect_group in range(len(rectangle_set) // rect_set_slice + 1):
+        begin_rect = rect_group*rect_set_slice
+        end_rect = (rect_group + 1)*rect_set_slice
+        end_rect = end_rect if end_rect < len(rectangle_set) else len(rectangle_set)
+        stdout.write('Calculating rectangles from %s to %s \n' % (begin_rect, end_rect))
 
-def calculateInterval(begin, end):
-    stdout.write('Calcilatinng interval (%i, %i)\n\n' % (begin, end))
-    for i in range(begin, end):
-        stdout.write('\r' + 'Processing %i image of %i' % (i, end-begin))
-        calculateRfd(i)
+        for set_group in range(dataset_raw.shape[0] // dataset_slice + 1):
+            begin_set = set_group*dataset_slice
+            end_set = (set_group + 1)*dataset_slice
+            end_set = end_set if end_set < dataset_raw.shape[0] else dataset_raw.shape[0]
+            temp_dataset_array[:(end_set - begin_set),:,:,:] = dataset_raw[begin_set : end_set, :, :, 1:]
+            stdout.write('    Calculating patches from %s to %s \n\n ' % (begin_set, end_set))
 
-#end = dataset.shape[0]
-end = 1000
-calculateInterval(0, end)
+            for rect_index in range(begin_rect, end_rect):
+                calcRectResponse(temp_dataset_array, begin_set, end_set, *rectangle_set[rect_index],
+                                 rect_index%rect_set_slice, temp_response_array)
 
-x = np.array([i for i in range(end)])
-y = np.zeros(end)
-for i in range(y.shape[0]):
-    if i in dataset_info:
-        y[i] = 1
+        for rect_index in range(temp_response_array.shape[0]):
+            print()
+            for chan_index in range(temp_response_array.shape[2]):
+                # Initialiaze binary search
+                delta = 0.25
+                threshold_middle = 0.5
+                bin_resp = binarResponses(temp_response_array[rect_index, :, chan_index], threshold_middle)
+                ones_zeros = calcOnesZeros(bin_resp, dataset_info)
+                accur_max = 0.5 * calcTP(ones_zeros) + 0.5 * calcTN(ones_zeros)
+                optima_threshold = threshold_middle
 
-plt.plot(x[:end], response_set[:end, 0])
-plt.plot(x[:end], y[:end])
+                while delta > 0.004:
+                    threshold_left, threshold_right = threshold_middle - delta, threshold_middle + delta
+                    for cur_threshold in (threshold_left, threshold_right):
+                        stdout.write('\rCalculating %s rectangle %s channel: threshold %s ' %
+                                     (rect_index, chan_index, cur_threshold))
+                        bin_resp = binarResponses(temp_response_array[rect_index, :, chan_index], cur_threshold)
+                        ones_zeros = calcOnesZeros(bin_resp, dataset_info)
+                        cur_accuracy = 0.5*calcTP(ones_zeros) + 0.5*calcTN(ones_zeros)
+                        if cur_accuracy > accur_max:
+                            accur_max = cur_accuracy
+                            optima_threshold = cur_threshold
+                    threshold_middle = optima_threshold
+                    delta = delta/2
 
-plt.show()
+                writeThresholdValue('Thresholds_Learned', *rectangle_set[rect_index], chan_index, optima_threshold)
 
-Utils.showImage(dataset[16,:12,:12,0])
-print('\n')
-print(response_set[2, 5])
+
+
--- RectangleCalculation.py
+++ RectangleCalculation.py
@@ -1,28 +1,63 @@
+import struct
 
-
-def candidateRects(height, width):
+def candidateRects(height, width, constraint=True):
+    """
+    Function takes height and width of original image patch and
+    calculates set of all possible rectangles can be taken.
+    Constraint makes possible to manage possible rectangles number, by constrainting their size (if set to True)
+    :return: rect set of kind ((ytopleft, xtopleft, ybottomright, xbottomright))
+    """
     rect_param = (height, width)
-
     rect_set = []
-
-    for i in range(rect_param[0]-1):
-        for j in range(rect_param[1]-1):
+    for i in range(rect_param[0]):
+        for j in range(rect_param[1]):
             for k in range(i, rect_param[0]):
                 for m in range(j, rect_param[1]):
-                    x0, y0 = i, j
-                    x1, y1 = k, m
-                    if (x1-x0) > 4 and (y1-y0) > 4 and \
-                        (x1-x0)%2 == 0 and (y1-y0)%2 ==0 and \
-                        (x1-x0)*(y1-y0) < 1024:
-                        rect_set.append( ( (x0, y0), (x1, y1) ) )
+                    y0, x0 = i, j
+                    y1, x1 = k, m
+                    if (x1-x0+1) > 4 and (y1-y0+1) > 4 and \
+                        (x1-x0+1)%2 == 0 and (y1-y0+1)%2 ==0 and \
+                        (x1-x0)*(y1-y0) < 1024 or not constraint:
+                        rect_set.append(  (y0, x0, y1, x1)  )
 
     return tuple(rect_set)
 
-dataset_cut = 5000
-for  i in range(312859 // dataset_cut +1):
-    begin = i*dataset_cut
-    if (i+1)*dataset_cut < 312859:
-        end = (i+1) * dataset_cut
-    else:
-        end = 312859
-    print((begin, end))
+def storeInitThreshold(rectangles,filename, init_threshold = 0.0):
+    """
+    Createsn little-endian binary file of kind:
+    (short: ytopleft, xtopleft, ybottomright, xbottomright, float: threshold)
+    """
+    with open(filename, 'wb') as binary_file:
+        for cur_rect in rectangles:
+            for cur_channel in range(8):
+                entry = struct.pack('<HHHHHf', *cur_rect, cur_channel, init_threshold)
+                binary_file.write(entry)
+                binary_file.flush()
+
+def writeThresholdValue(filename, rect_index, channel_index, threshold):
+    line_size = struct.calcsize('<HHHHHf')
+    offset = (rect_index*8 + channel_index)*line_size + struct.calcsize('<HHHHH')
+    with open(filename, 'wb') as binary_file:
+        binary_file.seek(offset)
+        entry = struct.pack('<f', threshold)
+        binary_file.write(entry)
+        binary_file.flush()
+
+def readEntry(filename, rect_index, channel_index):
+    line_size = struct.calcsize('<HHHHHf')
+    offset = (rect_index*8 + channel_index)*line_size
+    with open(filename, 'rb') as binary_file:
+        binary_file.seek(offset)
+        data = binary_file.read(line_size)
+        print(struct.unpack('<HHHHHf', data))
+
+if __name__ == '__main__':
+    rect_set = candidateRects(64, 64)
+    print(len(rect_set))
+    filename = "Thresholds_Learned"
+    #storeInitThreshold(rect_set, filename)
+    for i in range(5):
+        for j in range(8):
+            readEntry(filename, i, j)
+    #writeThresholdValue(filename, 8, 4, 0.783)
+    #readEntry(filename, 8, 4)
